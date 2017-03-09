815eef569c0c584d66c529721a053b4bac81329a
aamir.f <aamir.f@reactore.com>
2017-03-09 12:21:56 +0530

    added akka http with new methods

--- src/main/scala/Utilities/EmployeeRepository.scala
+++ src/main/scala/Utilities/EmployeeRepository.scala
@@ -2,10 +2,13 @@ package Utilities
 
 import Entities.{Employee, EmployeeTable}
 import slick.lifted.TableQuery
-abstract class EmployeeRepository
-  extends  BaseRepository[EmployeeTable, Employee](TableQuery[EmployeeTable]){
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
- // Employee(0L, "aamir", false)
+abstract class EmployeeRepository
+  extends BaseRepository[EmployeeTable, Employee](TableQuery[EmployeeTable]) {
+
+  // Employee(0L, "aamir", false)
 
   def insertItem(row: Employee) = {
     super.save(row)
@@ -13,6 +16,21 @@ abstract class EmployeeRepository
 
   def getEmployees = {
     super.getAll
+  }
+
+  def deleteRecord(id: Long) = {
+    super.deleteById(id)
+  }
+
+  def updateEmployee(id: Long, emp: Employee): Future[Int] = {
+    super.updateById(id, emp)
+  }
+
+  def getEmployeeByName(name: String): Future[Seq[Employee]] = {
+    for {
+      employees <- super.getAll
+      result = employees.filter(_.firstName == name)
+    } yield result
   }
 
 }
--- src/main/scala/rest/EmployeeRest.scala
+++ src/main/scala/rest/EmployeeRest.scala
@@ -8,7 +8,9 @@ import akka.http.scaladsl.server.Directives
 import akka.stream.ActorMaterializer
 import org.json4s.{DefaultFormats, Extraction}
 import org.json4s.jackson.JsonMethods._
+
 import scala.concurrent.ExecutionContext.Implicits.global
+
 class EmployeeRest extends Directives {
   implicit val system = ActorSystem.create("Test")
   implicit val materializer = ActorMaterializer()
@@ -31,6 +33,34 @@ class EmployeeRest extends Directives {
         }
       }
 
+    }
+  } ~ path("employee" / "employeeId" / LongNumber) { id =>
+    delete {
+      complete {
+        ImplEmployeeRepository.deleteRecord(id).map { result =>
+          HttpResponse(status = StatusCodes.OK, entity = HttpEntity(MediaTypes.`application/json`, compact(Extraction.decompose(result))))
+        }
+      }
+    } ~ put {
+      entity(as[String]) { data =>
+        complete {
+          val dd = parse(data).extract[Employee]
+          ImplEmployeeRepository.updateEmployee(id, dd).map { result =>
+            HttpResponse(status = StatusCodes.OK, entity = HttpEntity(MediaTypes.`application/json`, compact(Extraction.decompose(result))))
+          }
+        }
+      }
+    }
+  } ~ pathPrefix("employeeByName") {
+    path(Rest) { name =>
+      get {
+        complete {
+          ImplEmployeeRepository.getEmployeeByName(name).map { result =>
+            HttpResponse(status = StatusCodes.OK, entity = HttpEntity(MediaTypes.`application/json`, compact(Extraction.decompose(result))))
+          }
+
+        }
+      }
     }
   }
 }
